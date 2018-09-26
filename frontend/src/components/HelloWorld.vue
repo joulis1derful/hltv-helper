e6bec8f03abaa3c79f99c3cd6fa909d14b68938e
joulis1derful <joulis1derful@gmail.com>
2018-09-26 21:28:49 +0300

    Replaced anchors with checkboxes and labels and moved logos to another directory

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -4,89 +4,53 @@
     <h2>Essential Links</h2>
     <ul>
       <li>
-      <img src="../assets/team_logo/Astralis.svg"/>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Astralis
-        </a>
+        <img src="../assets/logos/Astralis.svg"/>
+        <input type="checkbox"/>
+        <label>Astralis</label>
       </li>
       <li>
-      <img src="../assets/team_logo/Navi.svg"/>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Na'Vi
-        </a>
+        <img src="../assets/logos/Navi.svg"/>
+        <input type="checkbox"/>
+        <label>Na'Vi</label>
       </li>
       <li>
-      <img src="../assets/team_logo/Faze.svg"/>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Faze clan
-        </a>
+        <img src="../assets/logos/Faze.svg"/>
+        <input type="checkbox"/>
+        <label>Faze clan</label>
       </li>
       <li>
-      <img src="../assets/team_logo/Liquid.svg"/>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Team Liquid
-        </a>
+        <img src="../assets/logos/Liquid.svg"/>
+        <input type="checkbox"/>
+        <label>Team Liquid</label>
       </li>
-      <br>
+
       <li>
-      <img src="../assets/team_logo/MIBR.svg"/>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          MIBR
-        </a>
+        <img src="../assets/logos/MIBR.svg"/>
+        <input type="checkbox"/>
+        <label>MIBR</label>
       </li>
     </ul>
 
     <ul>
       <li>
-      <img src="../assets/team_logo/Mousesports.svg"/>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          Mousesports
-        </a>
+        <img src="../assets/logos/Mousesports.svg"/>
+        <input type="checkbox"/>
+        <label>Mousesports</label>
       </li>
       <li>
-      <img src="../assets/team_logo/North.svg"/>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          North
-        </a>
+        <img src="../assets/logos/North.svg"/>
+        <input type="checkbox"/>
+        <label>North</label>
       </li>
       <li>
-      <img src="../assets/team_logo/Heroic.svg"/>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          Heroic
-        </a>
+        <img src="../assets/logos/Heroic.svg"/>
+        <input type="checkbox"/>
+        <label>Heroic</label>
       </li>
       <li>
-      <img src="../assets/team_logo/G2.svg"/>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          G2
-        </a>
+        <img src="../assets/logos/G2.svg"/>
+        <input type="checkbox"/>
+        <label>G2</label>
       </li>
     </ul>
   </div>
